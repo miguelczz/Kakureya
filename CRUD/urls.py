cdecdd7f785535e720b2f2478bec28352aa9a8e8
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2025-02-24 15:06:31 -0500

    Paginated

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -20,6 +20,7 @@ from kakureya import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home'),
-    path('signup/', views.signup, name = 'signup'),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks')
 ]
--- kakureya/views.py
+++ kakureya/views.py
@@ -1,31 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.http import HttpResponse
+from django.db import IntegrityError
 
-# Create your views here.
 
 def home(request):
-    
+
     return render(request, 'home.html')
-    
+
+
 def signup(request):
-    
+
     if request.method == 'GET':
-        form = UserCreationForm()
         return render(request, 'signup.html', {
-        'myform': form
-    })
+            'myform': UserCreationForm
+        })
+
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(username = request.POST['username'], password = request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1']
+                )
+
                 user.save()
-                return HttpResponse('Usuario creado')
-            
-            except:
-                return HttpResponse('Usuario existente')
-            
-        return HttpResponse('Las contraseñas no coinciden')
-        
-    
+                login(request, user)
+                return redirect('tasks')
+
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'myform': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })
+
+        return render(request, 'signup.html', {
+            'myform': UserCreationForm,
+            'error': 'Las contraseñas no coinciden'
+        })
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
