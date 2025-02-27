91b1e4541140968725ef48ad90db25991c58e9bc
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2025-02-26 22:07:32 -0500

    Admin sesion

--- kakureya/admin.py
+++ kakureya/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(Product, ProductAdmin)
