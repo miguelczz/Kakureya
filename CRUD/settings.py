379cf896d9bae0c2185d48f6d90d0f6232fc6865
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2025-03-02 19:08:22 -0500

    Debug config

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'    
+
 LOGIN_URL = '/signin'
 
 # Default primary key field type
