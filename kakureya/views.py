b140a719ed4713488e81276cc81f0ac5c271c1ae
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2025-03-09 18:33:03 -0500

    Views Fixed

--- kakureya/views.py
+++ kakureya/views.py
@@ -1,65 +1,106 @@
-from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authenticate
-
-from kakureya.models import Product
-from .services import (
-    register_user, authenticate_user, logout_user,
-    get_all_products, get_added_products, create_product,
-    get_product_by_id, update_product, delete_product,
-    add_product, quit_product
-)
+from django.db import IntegrityError
+from django.utils import timezone
 from .forms import ProductForm
+from .models import Product
+
 
 def home(request):
+
     return render(request, 'home.html')
 
+
 def signup(request):
+
     if request.method == 'GET':
-        return render(request, 'signup.html', {'form': UserCreationForm()})  # Instantiate the form correctly
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+
     else:
-        error = register_user(request)
-        if error:
-            return render(request, 'signup.html', {'form': UserCreationForm(), 'error': error})  # Same here
-        return redirect('products')
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1']
+                )
+
+                user.save()
+                login(request, user)
+                return redirect('products')
+
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'myform': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })
+
+        return render(request, 'signup.html', {
+            'myform': UserCreationForm,
+            'error': 'Las contraseñas no coinciden'
+        })
 
 @login_required
 def products(request):
-    return render(request, 'products.html', {'products': get_all_products()})
+    products = Product.objects.all()
+    return render(request, 'products.html', {
+        'products': products
+    })
 
 @login_required
+
 def products_added(request):
-    return render(request, 'products.html', {'products': get_added_products()})
+    products = Product.objects.filter(
+        added__isnull=False).order_by('-added')
+    return render(request, 'products.html', {
+        'products': products
+    })
 
 @login_required
+
 def create_product(request):
     if request.method == 'GET':
-        return render(request, 'create_product.html', {'form': ProductForm()})
-    
-    if request.method == 'POST':
-        form = ProductForm(request.POST)  # Recibe los datos del formulario
-        if form.is_valid():
+        return render(request, 'create_product.html', {
+            'form': ProductForm
+        })
+    elif request.method == 'POST':
+        try:
+            form = ProductForm(request.POST)
             new_product = form.save(commit=False)
-            new_product.user = request.user  # Asigna el usuario actual al producto
+            new_product.user = request.user
             new_product.save()
             return redirect('products')
-        else:
-            return render(request, 'create_product.html', {'form': form, 'error': "Error al crear el producto"})
+        except ValueError:
+            return render(request, 'create_product.html', {
+                'form': ProductForm,
+                'error': 'Campos invalidos'
+            })
 
 @login_required
+
 def product_detail(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
     if request.method == 'GET':
-        return render(request, 'product_detail.html', {'product': product, 'form': ProductForm(instance=product)})
+        product = get_object_or_404(Product, pk=product_id)
+        form = ProductForm(instance=product)
+        return render(request, 'product_detail.html', {'product': product, 'form': form})
     else:
-        error = update_product(request, product)
-        if error:
-            return render(request, 'product_detail.html', {'product': product, 'form': ProductForm(instance=product), 'error': error})
-        return redirect('products')
+        try:
+            product = get_object_or_404(Product, pk=product_id)
+            form = ProductForm(request.POST, instance=product)
+            form.save()
+            return redirect('products')
+        except ValueError:
+            return render(request, 'product_detail.html', {
+                'product': product,
+                'form': form,
+                'error': 'Campos invalidos'
+            })
 
 @login_required
+
 def add_product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
@@ -71,14 +112,16 @@ def add_product(request, product_id):
         return render(request, 'add_product.html', {'product': product})
 
 @login_required
+
 def delete_product(request, product_id):
-     product = get_object_or_404(Product, pk=product_id)
- 
-     if request.method == 'POST':
-         product.delete()
-         return redirect('products')
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        product.delete()
+        return redirect('products')
 
 @login_required
+
 def quit_product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
@@ -87,16 +130,28 @@ def quit_product(request, product_id):
         product.save()
         return redirect('products_added')
 
+@login_required
+
 def signout(request):
-    logout_user(request)
+    logout(request)
     return redirect('home')
+
 
 def signin(request):
     if request.method == 'GET':
-        return render(request, 'signin.html', {'form': AuthenticationForm()})  # Instantiate the form correctly
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
     else:
-        user = authenticate_user(request)
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password']
+        )
+
         if user is None:
-            return render(request, 'signin.html', {'form': AuthenticationForm(), 'error': 'Usuario o contraseña incorrectos'})  # Same here
-        login(request, user)
-        return redirect('home')
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
