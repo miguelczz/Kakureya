2a0d334eeae85ea30fb53083dbd01e0eec409c49
Miguel Zuluaga <jesus_zuluaga23232@elpoli.edu.co>
2025-03-31 13:08:28 -0500

    Change in float variables

--- kakureya/models.py
+++ kakureya/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField()
     stock = models.IntegerField(default=0)
     image = models.ImageField(upload_to='products', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
