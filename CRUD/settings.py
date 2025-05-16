ffb76d7cfdc91cbae338050c65dd5ceb832ec0f3
Miguel Zuluaga <jesus_zuluaga23232@elpoli.edu.co>
2025-05-16 06:58:07 -0500

    Upgraded facilities

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -37,6 +37,8 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,7 +135,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    
